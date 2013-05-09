7b87e38db41fe5141468f234d2b34f627288eef4
Hasan Karahan <hasan.karahan81@gmail.com>
2013-05-10 01:19:07 +0300

    minor

--- ticker.py
+++ ticker.py
@@ -56,14 +56,15 @@ def next_response (url: str) -> req.Response:
     except Exception as ex:
         print (ex, file=sys.stderr); return None
 
-def loop (out_keys: list, map_to: dict, interval: float, url: str,
+def loop (map_to: dict, out_keys: list, interval: float, url: str,
           verbose: bool=False) -> None:
 
-    def mapped (key: str) -> str:
-        return map_to[key] if key in map_to else key
+    def mapped (key: str, maps: dict) -> str:
+        return maps[key] if key in maps else key
 
-    def select (tick: dict) -> dict: return {
-        mapped (k): v for k, v in tick.items () if mapped (k) in out_keys
+    def select (tick: dict, maps: dict, keys: list=None) -> dict: return {
+        mapped (k, maps=maps): v for k, v in tick.items ()
+        if not keys or mapped (k, maps=maps) in keys
     }
 
     t0 = time.time ()
@@ -76,11 +77,11 @@ def loop (out_keys: list, map_to: dict, interval: float, url: str,
 
                 inp_tick = curr_response.json ()
                 inp_tick['timestamp'] = time.time ()
-                out_tick = select (inp_tick)
+                out_tick = select (inp_tick, map_to, keys=out_keys)
 
                 if verbose:
                     now = datetime.fromtimestamp (inp_tick['timestamp'])
-                    print ('[%s] %s' % (now, out_tick), file=sys.stderr)
+                    print ('[%s] %s' % (now, inp_tick), file=sys.stderr)
 
                 print (out_tick, file=sys.stdout); sys.stdout.flush ()
             last_response = curr_response
@@ -97,7 +98,9 @@ if __name__ == "__main__":
     args = get_arguments ()
     args.map_to = dict (args.map_to)
 
-    try: loop (args.out_keys, args.map_to, args.interval, args.url,
+    try: loop (
+        args.map_to, args.out_keys,
+        args.interval, args.url,
         verbose=args.verbose)
 
     except KeyboardInterrupt:
